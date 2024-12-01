71fc912a7e0bc937578188ca8e79924b7e21078d
Joy Dutta <dutta_joy@hotmail.com>
2024-12-01 23:18:28 +0000

    hands-on-tight-coupling-vs-loose-coupling

---
+++ 1-SpringExample/src/main/java/com/tight/coupling/TightCouplingExample.java
@@ -0,0 +1,10 @@
+package com.tight.coupling;
+
+import com.tight.coupling.UserManager;
+
+public class TightCouplingExample {
+    public static void main(String[] args) {
+        UserManager userManager = new UserManager();
+        System.out.println(userManager.getUserInfo());
+    }
+}
