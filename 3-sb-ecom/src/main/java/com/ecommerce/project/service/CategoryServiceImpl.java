16b9281c32f58c9b2ed316c33d776c9ec716a6d8
Joy Dutta <dutta_joy@hotmail.com>
2024-12-04 12:27:06 +0000

    69-responseentity-class

--- 3-sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ 3-sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,7 +1,10 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,13 +30,10 @@ public class CategoryServiceImpl implements CategoryService{
         Category category = categories.stream()
                 .filter(c ->
                         c.getCategoryId().equals(categoryId)
-                ).findFirst().orElse(null);
+                ).findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not found"));
 
         categories.remove(category);
-        if (category == null) {
-            return "Category Not Found";
-        }
-
-        return "Category with categoryId:" + categoryId + " has been deleted successfully";
+        return "Category with categoryId:" + categoryId + " has been deleted successfully!!";
     }
 }
