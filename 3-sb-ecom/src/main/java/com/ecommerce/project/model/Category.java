70d529cacf4e6e5101e4e877a2fb83c466f05ea9
Joy Dutta <dutta_joy@hotmail.com>
2024-12-05 17:50:53 +0000

    85-generation-types

--- 3-sb-ecom/src/main/java/com/ecommerce/project/model/Category.java
+++ 3-sb-ecom/src/main/java/com/ecommerce/project/model/Category.java
@@ -1,11 +1,15 @@
 package com.ecommerce.project.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
+//@Table(name = "categories") // Only if we want a different table name from the class name
 public class Category {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 
@@ -14,6 +18,12 @@ public class Category {
         this.categoryName = categoryName;
     }
 
+    /* It is considered a good practise to have a default constructor but it is NOT a strictly required
+       specification by JPA
+     */
+    public Category() {
+
+    }
     public Long getCategoryId() {
         return categoryId;
     }
