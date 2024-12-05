0a3237a9058dcf74aeb394d2166f0ce3839d7346
Joy Dutta <dutta_joy@hotmail.com>
2024-12-05 17:53:06 +0000

    86-defining-jpa-repositories

---
+++ 3-sb-ecom/src/main/java/com/ecommerce/project/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerce.project.repository;
+
+import com.ecommerce.project.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    // JpaRepository takes in 2 params: 1) Type of the Entity and the 2) Type of the primary key of the Entity
+
+}
