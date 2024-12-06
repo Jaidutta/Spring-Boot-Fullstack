665c7768f2a3590dd53b9f99b02048397953c88e
Joy Dutta <dutta_joy@hotmail.com>
2024-12-06 16:22:00 +0000

    94-lombok-action

--- 3-sb-ecom/src/main/java/com/ecommerce/project/model/Category.java
+++ 3-sb-ecom/src/main/java/com/ecommerce/project/model/Category.java
@@ -4,19 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
+@Entity(name = "categories")
 //@Table(name = "categories") // Only if we want a different table name from the class name
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
-
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
 
     /* It is considered a good practise to have a default constructor but it is NOT a strictly required
        specification by JPA.
@@ -25,23 +26,5 @@ public class Category {
        get the values in the GET request. These fields inside the database will also be Null if getters and
        setters are not added
      */
-    public Category() {
-
-    }
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 
 }
